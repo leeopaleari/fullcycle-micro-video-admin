6dfc2529db40f9884324345cd481cd8faabc6c59
Leonardo Paleari <leologe@gmail.com>
2025-03-14 08:34:51 -0300

    Implement categoryInMemoryRepository

--- src/core/category/infra/db/category-in-memory.repository.ts
+++ src/core/category/infra/db/category-in-memory.repository.ts
@@ -1,12 +1,41 @@
 import { Category } from "@core/category/domain/category.entity";
+import { SortDirection } from "@core/shared/domain/repository/search-params";
 import { Uuid } from "@core/shared/domain/value-objects/uuid.vo";
-import { InMemoryRepository } from "@core/shared/infra/db/in-memory/in-memory.repository";
+import {
+  InMemoryRepository,
+  InMemorySearchableRepository,
+} from "@core/shared/infra/db/in-memory/in-memory.repository";
 
-export class CategoryInMemoryRepository extends InMemoryRepository<
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<
   Category,
   Uuid
 > {
+  sortableFields: string[] = ["name", "createdAt"];
+
+  protected async applyFilter(
+    items: Category[],
+    filter: string
+  ): Promise<Category[]> {
+    if (!filter) {
+      return items;
+    }
+
+    return items.filter((i) =>
+      i.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  }
+
   getEntity(): new (...args: any[]) => Category {
     return Category;
+  }
+
+  protected applySort(
+    items: Category[],
+    sort: string | null,
+    sort_dir: SortDirection | null
+  ): Category[] {
+    return sort
+      ? super.applySort(items, sort, sort_dir)
+      : super.applySort(items, "createdAt", "desc");
   }
 }
