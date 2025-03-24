380be59d6ce4ec0273d084f2d2e388f2ffd20a80
Leonardo Paleari <leologe@gmail.com>
2025-03-24 00:36:46 +0000

    DeleteCategoryUseCase

---
+++ src/core/category/application/__tests__/int/delete-category.use-case.int-spec.ts
@@ -0,0 +1,35 @@
+import { Category } from "@core/category/domain/category.entity";
+import { CategorySequelizeRepository } from "@core/category/infra/db/sequelize/category-sequelize.repository";
+import { CategoryModel } from "@core/category/infra/db/sequelize/category.model";
+import { NotFoundError } from "@core/shared/domain/errors/not-found.error";
+import { Uuid } from "@core/shared/domain/value-objects/uuid.vo";
+import { setupSequelize } from "@core/shared/infra/testing/helpers";
+import { DeleteCategoryUseCase } from "../../delete-category.use-case";
+
+describe("DeleteCategoryUseCase Integration Tests", () => {
+  let useCase: DeleteCategoryUseCase;
+  let repository: CategorySequelizeRepository;
+
+  setupSequelize({ models: [CategoryModel] });
+
+  beforeEach(() => {
+    repository = new CategorySequelizeRepository(CategoryModel);
+    useCase = new DeleteCategoryUseCase(repository);
+  });
+
+  it("should throws error when entity not found", async () => {
+    const categoryId = new Uuid();
+    await expect(() => useCase.execute({ id: categoryId.id })).rejects.toThrow(
+      new NotFoundError(categoryId.id, Category)
+    );
+  });
+
+  it("should delete a category", async () => {
+    const category = Category.fake().aCategory().build();
+    await repository.insert(category);
+    await useCase.execute({
+      id: category.categoryId.id,
+    });
+    await expect(repository.findById(category.categoryId)).resolves.toBeNull();
+  });
+});
---
+++ src/core/category/application/delete-category.use-case.ts
@@ -0,0 +1,21 @@
+import { IUseCase } from "@core/shared/application/use-case.interface";
+import { Uuid } from "@core/shared/domain/value-objects/uuid.vo";
+import { ICategoryRepository } from "../domain/category.repository";
+
+export class DeleteCategoryUseCase
+  implements IUseCase<DeleteCategoryInput, DeleteCategoryOutput>
+{
+  constructor(private readonly categoryRepo: ICategoryRepository) {}
+
+  async execute(input: DeleteCategoryInput): Promise<DeleteCategoryOutput> {
+    const uuid = new Uuid(input.id);
+
+    await this.categoryRepo.delete(uuid);
+  }
+}
+
+export type DeleteCategoryInput = {
+  id: string;
+};
+
+type DeleteCategoryOutput = void;
